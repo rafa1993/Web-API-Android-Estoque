64d4e634a3cbcffe020dc3b1ea99fcf3236ba6a0
Rafaela Hristidis Morais <rafaelamorais@brq.com>
2021-03-31 10:26:22 -0300

    integracao busca de produtos

--- app/src/main/java/br/com/alura/estoque/ui/activity/ListaProdutosActivity.java
+++ app/src/main/java/br/com/alura/estoque/ui/activity/ListaProdutosActivity.java
@@ -1,28 +1,21 @@
 package br.com.alura.estoque.ui.activity;
 
 import android.os.Bundle;
-import android.widget.Toast;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.io.IOException;
-import java.util.List;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import br.com.alura.estoque.R;
 import br.com.alura.estoque.asynctask.BaseAsyncTask;
 import br.com.alura.estoque.database.EstoqueDatabase;
 import br.com.alura.estoque.database.dao.ProdutoDAO;
 import br.com.alura.estoque.model.Produto;
-import br.com.alura.estoque.retrofit.EstoqueRetrofit;
-import br.com.alura.estoque.retrofit.service.ProdutoService;
+import br.com.alura.estoque.repository.ProdutoRepository;
 import br.com.alura.estoque.ui.dialog.EditaProdutoDialog;
 import br.com.alura.estoque.ui.dialog.SalvaProdutoDialog;
 import br.com.alura.estoque.ui.recyclerview.adapter.ListaProdutosAdapter;
-import retrofit2.Call;
-import retrofit2.Response;
 
 public class ListaProdutosActivity extends AppCompatActivity {
 
@@ -42,32 +35,8 @@ public class ListaProdutosActivity extends AppCompatActivity {
         EstoqueDatabase db = EstoqueDatabase.getInstance(this);
         dao = db.getProdutoDAO();
 
-        buscaProdutos();
-    }
-
-    private void buscaProdutos() {
-        ProdutoService service = new EstoqueRetrofit().getProdutoService();
-        Call<List<Produto>> call = service.buscaTodos();
-
-        new BaseAsyncTask<>(() -> {
-           //execução síncrona
-            try {
-                Response<List<Produto>> resposta = call.execute();
-                List<Produto> produtosNovos = resposta.body();
-                return produtosNovos;
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }, produtosNovos -> {
-            if (produtosNovos != null){
-                adapter.atualiza(produtosNovos);
-            } else {
-                Toast.makeText(this, "Não foi possível achar os produtos", Toast.LENGTH_SHORT).show();
-            }
-        }).execute();
-
-        //new BaseAsyncTask<>(dao::buscaTodos, resultado -> adapter.atualiza(resultado)).execute();
+        ProdutoRepository repository = new ProdutoRepository(dao);
+        repository.buscaProdutos(adapter::atualiza);
     }
 
     private void configuraListaProdutos() {
